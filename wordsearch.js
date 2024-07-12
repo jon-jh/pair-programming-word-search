dfb6810351b65857ee397ded0c08dc42c9305623
jon-jh <hiebert456@gmail.com>
2024-07-12 15:31:54 -0600

    minor comment update

--- wordsearch.js
+++ wordsearch.js
@@ -1,34 +1,36 @@
 const wordSearch = (letters, word) => {
-    const horizontalJoin = letters.map(ls => ls.join(''));
-    for (let l of horizontalJoin) {
-        if (l.includes(word)) {
-            return true;
-        }
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  for (let l of horizontalJoin) {
+    if (l.includes(word)) {
+      return true;
+    }
+  }
+
+  const columnLength = letters.length;
+  const rowsLength = letters[0].length;
+
+  // Insert array transpose function here, flip array, and run horizontal word search again to accomplish searching through the array vertically.
+
+  let newMatrix = [];
+
+  // Create an empty array for each column in the original matrix
+  for (let i = 0; i < rowsLength; i++) {
+    newMatrix[i] = [];
+    // Add the relative part of the original matrix
+    for (let j = 0; j < columnLength; j++) {
+      newMatrix[i][j] = letters[j][i];
     }
 
-    const columnLength = letters.length;
-    const rowsLength = letters[0].length;
-    //empty matrix init
-    let newMatrix = [];
+  }
 
-    //create an empty array for each column in the original matrix
-    for (let i = 0; i < rowsLength; i++) {
-        newMatrix[i] = [];
-        // add the relative part of the original matrix
-        for (let j = 0; j < columnLength; j++) {
-            newMatrix[i][j] = letters[j][i];
-        }
-
+  const verticalJoin = newMatrix.map(ls => ls.join(''));
+  for (let l of verticalJoin) {
+    if (l.includes(word)) {
+      return true;
     }
+  }
 
-    const verticalJoin = newMatrix.map(ls => ls.join(''));
-    for (let l of verticalJoin) {
-        if (l.includes(word)) {
-            return true;
-        }
-    }
-
-    return false;
+  return false;
 
 };
 
