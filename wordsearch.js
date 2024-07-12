8bc38e10c82432548dc0057a7b5aa3d90db2a6e0
jon-jh <hiebert456@gmail.com>
2024-07-12 15:28:50 -0600

    insert previously created matrix transpose function to search search vertical columns of array

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,35 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const wordSearch = (letters, word) => {
+    const horizontalJoin = letters.map(ls => ls.join(''));
+    for (let l of horizontalJoin) {
+        if (l.includes(word)) {
+            return true;
+        }
     }
-}
 
-module.exports = wordSearch
+    const columnLength = letters.length;
+    const rowsLength = letters[0].length;
+    //empty matrix init
+    let newMatrix = [];
+
+    //create an empty array for each column in the original matrix
+    for (let i = 0; i < rowsLength; i++) {
+        newMatrix[i] = [];
+        // add the relative part of the original matrix
+        for (let j = 0; j < columnLength; j++) {
+            newMatrix[i][j] = letters[j][i];
+        }
+
+    }
+
+    const verticalJoin = newMatrix.map(ls => ls.join(''));
+    for (let l of verticalJoin) {
+        if (l.includes(word)) {
+            return true;
+        }
+    }
+
+    return false;
+
+};
+
+module.exports = wordSearch;
